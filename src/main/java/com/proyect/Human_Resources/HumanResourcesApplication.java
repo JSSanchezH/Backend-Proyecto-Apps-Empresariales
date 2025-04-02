a2df82d2ea3d8cc964719526a439e5c4ea8b5e1a
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-01 20:46:46 -0500

    feat: spring project initialized

---
+++ src/main/java/com/proyect/Human_Resources/HumanResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.Human_Resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesApplication.class, args);
+	}
+
+}

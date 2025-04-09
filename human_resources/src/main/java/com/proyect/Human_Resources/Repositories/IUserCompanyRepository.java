c715e84ac74eff2c1cd070be96987266efff2bca
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-09 15:57:03 -0500

    feat: UserCompany repository added

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IUserCompanyRepository.java
@@ -0,0 +1,9 @@
+package com.proyect.Human_Resources.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyect.Human_Resources.models.UserCompany;
+
+public interface IUserCompanyRepository extends JpaRepository<UserCompany, Long> {
+    // Custom query methods can be defined here if needed
+}

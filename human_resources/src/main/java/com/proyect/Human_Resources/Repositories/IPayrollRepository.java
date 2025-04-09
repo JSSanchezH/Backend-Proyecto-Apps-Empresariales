f27b4c2136fba06d6afecb734727602750d5537e
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-09 16:03:39 -0500

    feat: Payroll  repository added

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IPayrollRepository.java
@@ -0,0 +1,9 @@
+package com.proyect.Human_Resources.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyect.Human_Resources.models.Payroll;
+
+public interface IPayrollRepository extends JpaRepository<Payroll, Long> {
+    // Custom query methods can be defined here if needed
+}

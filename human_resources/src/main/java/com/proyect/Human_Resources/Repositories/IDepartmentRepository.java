d7d5cf2c621874587c19e27b17fdd91d40f310e8
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-09 16:12:55 -0500

    feat: Department repository added

--- human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IDepartmentRepository.java
+++ human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IDepartmentRepository.java
@@ -2,8 +2,8 @@ package com.proyect.Human_Resources.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.proyect.Human_Resources.models.Department;
+
 public interface IDepartmentRepository extends JpaRepository<Department, Long> {
     // Custom query methods can be defined here if needed
-
-    
 }

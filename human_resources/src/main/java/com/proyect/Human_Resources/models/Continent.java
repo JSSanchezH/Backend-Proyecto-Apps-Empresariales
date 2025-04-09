e00e710acb567f32200e0b55dfa074b9faca6e2b
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-09 00:48:14 -0500

    feat: Continent class added

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/models/Continent.java
@@ -0,0 +1,40 @@
+package com.proyect.Human_Resources.models;
+
+// Importing necessary JPA annotations for entity mapping
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "continents")
+// Defining the Continent entity and mapping it to the "continents" table in the
+// database
+public class Continent {
+
+    @Id // Primary key attribute that is mapped to the id column of the continents table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generating the ID value
+    private Long id; // Unique identifier for the continent
+
+    @Column(name = "name", nullable = false, length = 50) // Column annotation to specify the mapping of the name
+                                                          // attribute to the database
+    // The name column is not nullable and has a maximum length of 50 characters
+    private String name; // Name of the continent
+
+    // Getters and Setters for accessing and modifying the attributes of the
+    // Continent class
+
+    public Long getId() {
+        return id; // Returns the unique identifier of the continent
+    }
+
+    public void setId(Long id) {
+        this.id = id; // Sets the unique identifier of the continent
+    }
+
+    public String getName() {
+        return name; // Returns the name of the continent
+    }
+
+    public void setName(String name) {
+        this.name = name; // Sets the name of the continent
+    }
+
+}

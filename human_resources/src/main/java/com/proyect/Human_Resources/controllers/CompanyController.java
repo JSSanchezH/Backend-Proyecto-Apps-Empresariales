43d64d578d3394fb7e9e8604f084df4d6e994cf5
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-20 16:56:14 -0500

    feat: Company service and Company controller are working

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/controllers/CompanyController.java
@@ -0,0 +1,55 @@
+package com.proyect.Human_Resources.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyect.Human_Resources.models.Company;
+import com.proyect.Human_Resources.services.CompanyService;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public ArrayList<Company> getCompanies() {
+        return companyService.getCompanies();
+    }
+
+    @PostMapping
+    public Company saveCompany(@RequestBody Company company) {
+        return companyService.saveCompany(company);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Company> getCompanyById(@PathVariable("id") long id) {
+        return companyService.getCompanyById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Company updateCompany(@RequestBody Company company, @PathVariable("id") long id) {
+        return companyService.updateCompany(company, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCompany(@PathVariable("id") long id) {
+        boolean ok = companyService.deleteCompany(id);
+        if (ok) {
+            return "Company deleted successfully";
+        } else {
+            return "Error deleting company";
+        }
+    }    
+}
---
+++ human_resources/src/main/java/com/proyect/Human_Resources/services/CompanyService.java
@@ -0,0 +1,50 @@
+package com.proyect.Human_Resources.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyect.Human_Resources.Repositories.ICompanyRepository;
+import com.proyect.Human_Resources.models.Company;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private ICompanyRepository companyRepository;
+
+    public ArrayList<Company> getCompanies() {
+        return (ArrayList<Company>) companyRepository.findAll();
+    }
+
+    public Company saveCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
+    public Optional<Company> getCompanyById(long id) {
+        return companyRepository.findById(id);
+    }
+
+    public Company updateCompany(Company company, long id) {
+        Company companyToUpdate = companyRepository.findById(id).get();
+        companyToUpdate.setName(company.getName());
+        companyToUpdate.setNit(company.getNit());
+        companyToUpdate.setAddress(company.getAddress());
+        companyToUpdate.setEmail(company.getEmail());
+        companyToUpdate.setTypeIndustry(company.getTypeIndustry());
+        companyToUpdate.setUrlLogo(company.getUrlLogo());
+        return companyRepository.save(companyToUpdate);
+    }
+
+    public boolean deleteCompany(long id) {
+        try {
+            companyRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+}

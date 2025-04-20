60c746e1594821dd19799cf1df64355709f97fef
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-20 16:06:51 -0500

    feat: PaymentMerhod service and PaymentMethod controller are working

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/controllers/Payment_MethodController.java
@@ -0,0 +1,55 @@
+package com.proyect.Human_Resources.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyect.Human_Resources.models.Payment_Method;
+import com.proyect.Human_Resources.services.Payment_MethodService;
+
+@RestController
+@RequestMapping("/payment-methods")
+public class Payment_MethodController {
+
+    @Autowired
+    private Payment_MethodService paymentMethodService;
+
+    @GetMapping
+    public ArrayList<Payment_Method> getPayment_Methods() {
+        return paymentMethodService.getPayment_Methods();
+    }
+
+    @PostMapping
+    public Payment_Method savePayment_Method(@RequestBody Payment_Method paymentMethod) {
+        return paymentMethodService.savePayment_Method(paymentMethod);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Payment_Method> getPayment_MethodById(@PathVariable("id") long id) {
+        return paymentMethodService.getPayment_MethodById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Payment_Method updatePayment_Method(@RequestBody Payment_Method paymentMethod, @PathVariable("id") long id) {
+        return paymentMethodService.updatePayment_Method(paymentMethod, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deletePayment_Method(@PathVariable("id") long id) {
+        boolean ok = paymentMethodService.deletePayment_Method(id);
+        if (ok) {
+            return "Payment method deleted successfully";
+        } else {
+            return "Error deleting payment method";
+        }
+    }
+}
---
+++ human_resources/src/main/java/com/proyect/Human_Resources/services/Payment_MethodService.java
@@ -0,0 +1,45 @@
+package com.proyect.Human_Resources.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyect.Human_Resources.Repositories.IPaymentMethodRepository;
+import com.proyect.Human_Resources.models.Payment_Method;
+
+@Service
+public class Payment_MethodService {
+
+    @Autowired
+    private IPaymentMethodRepository paymentMethodRepository;
+
+    public ArrayList<Payment_Method> getPayment_Methods() {
+        return (ArrayList<Payment_Method>) paymentMethodRepository.findAll();
+    }
+
+    public Payment_Method savePayment_Method(Payment_Method paymentMethod) {
+        return paymentMethodRepository.save(paymentMethod);
+    }
+
+    public Optional<Payment_Method> getPayment_MethodById(long id) {
+        return paymentMethodRepository.findById(id);
+    }
+
+    public Payment_Method updatePayment_Method(Payment_Method paymentMethod, long id) {
+        Payment_Method paymentMethodToUpdate = paymentMethodRepository.findById(id).get();
+        paymentMethodToUpdate.setName(paymentMethod.getName());
+        return paymentMethodRepository.save(paymentMethodToUpdate);
+    }
+
+    public boolean deletePayment_Method(long id) {
+        try {
+            paymentMethodRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+}

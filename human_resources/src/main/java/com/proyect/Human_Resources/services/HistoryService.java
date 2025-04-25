8668228bcda9455020fac2d674b33d98c8618233
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-25 13:35:22 -0500

    feat: History repository, history service and history controller are working

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/services/HistoryService.java
@@ -0,0 +1,51 @@
+package com.proyect.Human_Resources.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyect.Human_Resources.Repositories.IHistoryRepository;
+import com.proyect.Human_Resources.models.History;
+
+@Service // Indicates that this class is a service component in the Spring framework
+public class HistoryService {
+
+    @Autowired 
+    private IHistoryRepository historyRepository; // Injecting the IHistoryRepository dependency
+
+    // Method to save a history record
+
+    public ArrayList<History> getHistories() {
+        return (ArrayList<History>) historyRepository.findAll(); // Saves the history record to the database and returns the saved record
+    }
+
+    public History saveHistory(History history) {
+        return historyRepository.save(history); // Saves the history record to the database and returns the saved record
+    }
+
+    // Method to retrieve a history record by its ID
+    public Optional<History> getHistoryById(long id) {
+        return historyRepository.findById(id); // Retrieves the history record by its ID
+    }
+
+    // Method to update a history record
+
+    public History updateHistory(History history, long id) {
+        History historyToUpdate = historyRepository.findById(id).get(); // Retrieves the history record to update
+        historyToUpdate.setEndDate(history.getEndDate()); // Updates the end date of the history record
+        historyToUpdate.setReason(history.getReason()); // Updates the reason of the history record
+        return historyRepository.save(historyToUpdate); // Saves the updated history record and returns it
+    }
+
+    public boolean deleteHistory(long id) {
+        try {
+            historyRepository.deleteById(id); // Deletes the history record by its ID
+            return true; // Returns true if deletion was successful
+        } catch (Exception e) {
+            return false; // Returns false if there was an error during deletion
+        }
+    }
+
+}

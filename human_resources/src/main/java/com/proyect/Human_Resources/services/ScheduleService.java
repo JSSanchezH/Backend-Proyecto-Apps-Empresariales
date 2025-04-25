464266d016b2894b6cf133170fc4cc21d63a4418
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-25 14:42:42 -0500

    feat: Schedule service ended and schedule controller is working

---
+++ human_resources/src/main/java/com/proyect/Human_Resources/services/ScheduleService.java
@@ -0,0 +1,49 @@
+package com.proyect.Human_Resources.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyect.Human_Resources.Repositories.IScheduleReposiory;
+import com.proyect.Human_Resources.models.Schedule;
+
+@Service // Indicates that this class is a service component in the Spring framework
+public class ScheduleService {
+
+    @Autowired 
+    private IScheduleReposiory scheduleRepository; // Injecting the IScheduleRepository dependency
+
+    public ArrayList<Schedule> getSchedules() {
+        return (ArrayList<Schedule>) scheduleRepository.findAll(); // Retrieves all schedule records from the database
+    }
+
+    public Schedule saveSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule); // Saves the schedule record to the database and returns the saved record
+    }
+
+    public Optional<Schedule> getScheduleById(long id) {
+        return scheduleRepository.findById(id); // Retrieves the schedule record by its ID
+    }
+
+    public Schedule updateSchedule(Schedule schedule, long id) {
+        Schedule scheduleToUpdate = scheduleRepository.findById(id).get(); // Retrieves the schedule record to update
+        scheduleToUpdate.setStartTime(schedule.getStartTime()); // Updates the start time of the schedule record
+        scheduleToUpdate.setEndTime(schedule.getEndTime()); // Updates the end time of the schedule record
+        scheduleToUpdate.setBreakStart(schedule.getBreakStart()); // Updates the break start time of the schedule record
+        scheduleToUpdate.setBreakEnd(schedule.getBreakEnd()); // Updates the break end time of the schedule record
+        scheduleToUpdate.setEmployee(schedule.getEmployee()); // Updates the employee associated with the schedule record
+        return scheduleRepository.save(scheduleToUpdate); // Saves the updated schedule record and returns it
+    }
+
+    public boolean deleteSchedule(long id) {
+        try {
+            scheduleRepository.deleteById(id); // Deletes the schedule record by its ID
+            return true; // Returns true if deletion was successful
+        } catch (Exception e) {
+            return false; // Returns false if there was an error during deletion
+        }
+    }
+
+}

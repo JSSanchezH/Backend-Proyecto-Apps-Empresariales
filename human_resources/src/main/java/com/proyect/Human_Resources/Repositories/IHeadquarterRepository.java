78ca2e2bf4d8f639d91ff47b62b81408f738e3dd
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-14 12:38:51 -0500

    feat: Headquarter repository,services and controller were updated

--- human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IHeadquarterRepository.java
+++ human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IHeadquarterRepository.java
@@ -1,10 +1,14 @@
 package com.proyect.Human_Resources.Repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.proyect.Human_Resources.models.Headquarter;
 
 public interface IHeadquarterRepository extends JpaRepository<Headquarter, Long> {
     // Custom query methods can be defined here if needed
+
+    public ArrayList<Headquarter> findByCompanyNit(long nit);
     
 }

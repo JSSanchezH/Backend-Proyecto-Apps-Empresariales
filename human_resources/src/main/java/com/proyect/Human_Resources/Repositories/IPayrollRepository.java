811ed6aea9a79be89adf5437d6a512652eb6c566
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-14 13:38:25 -0500

    feat: Payroll repository,services and controller were updated

--- human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IPayrollRepository.java
+++ human_resources/src/main/java/com/proyect/Human_Resources/Repositories/IPayrollRepository.java
@@ -1,9 +1,13 @@
 package com.proyect.Human_Resources.Repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.proyect.Human_Resources.models.Payroll;
 
 public interface IPayrollRepository extends JpaRepository<Payroll, Long> {
     // Custom query methods can be defined here if needed
+
+    public ArrayList<Payroll> findByEmployeeDepartmentHeadquarterCompanyNit(long nit); // Find payrolls by company NIT
 }
